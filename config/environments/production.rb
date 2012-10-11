81be5368120b7e1cad62e0b31659b871462930ff
Chris Tingley <chris.tingley@conjure.co.uk>
2012-10-11 15:20:43 +0100

    Setup complete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Bennett::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :sendmail
-  HOST = "ci.belighted.com"
+  HOST = "build.conjure.co.uk"
   config.action_mailer.default_url_options = {
     :host => HOST,
   }
